d3e1bcc68c33715da3d775e38ef2a69e3552b331
John Joo <johnjoo1@users.noreply.github.com>
2018-12-12 14:36:41 -0800

    Update get_word2vec.sh
    Using Slimmed version of the model

--- src/models/get_word2vec.sh
+++ src/models/get_word2vec.sh
@@ -1,3 +1,9 @@
-wget https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz
-gunzip -f GoogleNews-vectors-negative300.bin.gz
-mv GoogleNews-vectors-negative300.bin data/external/GoogleNews-vectors-negative300.bin
+#!/bin/bash
+
+# wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
+wget https://github.com/eyaler/word2vec-slim/raw/master/GoogleNews-vectors-negative300-SLIM.bin.gz
+echo "Decompressing..."
+gunzip -f -v GoogleNews-vectors-negative300-SLIM.bin.gz
+echo "Decompressed. Moving..."
+mv GoogleNews-vectors-negative300-SLIM.bin /mnt/data/external/GoogleNews-vectors-negative300-SLIM.bin
+echo "Move completed."
